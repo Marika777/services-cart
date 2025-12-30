211b45380bba60357012ae43a15bc3575ef481ab
Meidra Marika <marika.meidra2003@gmail.com>
2025-12-30 12:34:44 +0300

    data

---
+++ src/data/servicesData.ts
@@ -0,0 +1,19 @@
+import type { Service } from '../types/services';
+
+export const services: Service[] = [
+  {
+    id: 1,
+    name: 'Service 1',
+    price: 100,
+  },
+  {
+    id: 2,
+    name: 'Service 2',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: 'Service 3',
+    price: 300,
+  },
+];
