8c0a918ec10414d6d77066bdd85038d07b48d3ac
Meidra Marika <marika.meidra2003@gmail.com>
2025-12-30 12:52:25 +0300

    addToCart in ServiceCard

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,3 +1,4 @@
+import { useCart } from "../hooks/useCart";
 import type { Service } from "../types/services";
 
 interface ServiceCard {
@@ -5,12 +6,13 @@ interface ServiceCard {
 }
 
 export const ServiceCard = ({ service }: ServiceCard) => {
+  const {addToCart} = useCart()
 
   return (
     <div className="service-card">
       <h2>{service.name}</h2>
       <p className="price">{service.price.toLocaleString()} ₽</p>
-      <button>Добавить</button>
+      <button onClick={() => addToCart(service)}>Добавить</button>
     </div>
   )
 }
